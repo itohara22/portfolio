26c8fa327efb0a0b6b1a543029769502daf986df
itohara22 <ito.k23018@gmail.com>
2024-01-09 20:00:59 +0530

    project section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import { Projects } from "./components/Projects";
 
 export default function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Projects />
     </>
   );
 }
---
+++ src/components/Projects.tsx
@@ -0,0 +1,8 @@
+export const Projects = () => {
+  return (
+    <div className="w-full min-h-screen flex items-start gap-20">
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
