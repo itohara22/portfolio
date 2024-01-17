4d30a570e107a213619cbb20f2ca7f68a0c9bacd
itohara22 <ito.k23018@gmail.com>
2024-01-17 15:08:29 +0530

    title

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
 import About from "./components/About";
 import Connect from "./components/Connect";
 import Hero from "./components/Hero";
 import { Projects } from "./components/Projects";
+import Lenis from "@studio-freight/lenis/types";
 
 export default function App() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
   return (
     <>
       <Hero />
